00234d8bcb272a66bb616e94e6c719a03b7a9792
VivekPandey09032002 <vivekpandeykcc09032002@gmail.com>
2022-12-23 19:12:36 +0530

    try

--- backend/app.js
+++ backend/app.js
@@ -1,38 +1,42 @@
-const cookieParser = require('cookie-parser')
-const express = require('express')
+const cookieParser = require("cookie-parser")
+const express = require("express")
 const app = express()
-const cors = require('cors');
-const errorMiddleware = require('./middleware/error')
+const cors = require("cors")
+const errorMiddleware = require("./middleware/error")
 
-app.use(cors({
-    origin : ["http://192.168.1.5:5173", "http://localhost:5173"],
-    credentials : true
-}))
+app.use(
+  cors({
+    origin: ["http://192.168.1.5:5173", "http://localhost:5173"],
+    credentials: true,
+  })
+)
 app.use(express.json())
 app.use(cookieParser())
 
 //routes imports
-const product = require('./routes/productRoute')
-const user = require('./routes/userRoute')
+const product = require("./routes/productRoute")
+const user = require("./routes/userRoute")
 const order = require("./routes/orderRoute")
 //product route
-app.use('/api/v1',order)
-app.use('/api/v1',product)
-app.use('/api/v1',user)
+app.use("/api/v1", order)
+app.use("/api/v1", product)
+app.use("/api/v1", user)
 
 //home page route connect client
 
-if(process.env.NODE_ENV=='production'){
-   
+if (process.env.NODE_ENV == "production") {
+  const path = require("path")
+  app.get("/", (req, res) => {
+    app.use(
+      express.static(path.join(__dirname, "..", "frontend", "dist", "assets"))
+    )
+    res.sendFile(
+      path.resolve(__dirname, "..", "frontend", "dist", "index.html")
+    )
+  })
 }
-const path = require('path')
-app.get('/',(req,res) => {
-    app.use(express.static(path.join(__dirname,'..','frontend','dist','assets')))
-    res.sendFile(path.resolve(__dirname,'..','frontend','dist','index.html'))
-})
 
 //middleware for error -> last middleware
 app.use(errorMiddleware)
 
 module.exports = app
-
